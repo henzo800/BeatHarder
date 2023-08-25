ffe3dc0e8bc49a5ef53447496b4e6dd963745674
henzo <19755110+henzo800@users.noreply.github.com>
2023-08-25 22:54:54 +1000

    Fix merge conflict

--- Assets/Scripts/FPS.cs
+++ Assets/Scripts/FPS.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class FPS : MonoBehaviour
 {
-<<<<<<< HEAD
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-=======
     public Transform targetPosition;
     
     // Update is called once per frame
     void Update()
     {
         transform.position = targetPosition.position;
->>>>>>> 3d9086dd0ebcf5f77778eb43723d7e183d845a9f
     }
 }
