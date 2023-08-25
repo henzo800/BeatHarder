91f34a4d2d3dbc1268055e509a3f9de582cc1544
henzo <19755110+henzo800@users.noreply.github.com>
2023-08-25 22:05:38 +1000

    A

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 0.05f;
+    
+    // Update is called once per frame
+    void Update() {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+        
+        Vector3 moveDirection = new Vector3(xMove, 0.0f, zMove);
+
+        transform.position += moveDirection * speed;
+    }
+}
