7a5452c8d4423fdb7c5338b4434aa9f6b8406752
henzo <19755110+henzo800@users.noreply.github.com>
2023-08-26 10:47:07 +1000

    NavMesh Stuff

--- Assets/Scripts/Enemys/FollowingMinionController.cs
+++ Assets/Scripts/Enemys/FollowingMinionController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FollowingMinionController : MonoBehaviour
 {
+    public CharacterData characterData;
     // Start is called before the first frame update
     void Start()
     {
