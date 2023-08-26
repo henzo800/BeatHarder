857c59d7a133a7579a987e5a05d96e8851d4e616
henzo <19755110+henzo800@users.noreply.github.com>
2023-08-26 13:57:25 +1000

    Added Bullets

--- Assets/Scripts/Enemys/TurretMinionController.cs
+++ Assets/Scripts/Enemys/TurretMinionController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TurretMinionController : MonoBehaviour
 {
     public Transform mount;
+    public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
